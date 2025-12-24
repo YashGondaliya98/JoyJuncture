05e3e3f1c581015a3b8722c694d294e7baa8b4f5
YashGondaliya98 <yashgondaliya98@gmail.com>
2025-12-24 12:07:48 +0530

    GAME STORE PAGE CREATED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/homepage';
 import ReviewPage from './components/reviewpage';
+import GameStore from './components/gamestore';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/reviews" element={<ReviewPage />} />
+      <Route path="/gamestore" element={<GameStore />} />
     </Routes>
   );
 }

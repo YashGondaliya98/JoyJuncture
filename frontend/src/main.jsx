58722fe2eedd57b21948629dbc71784f54bc9e83
Prince Kalsariya <kalsariyaprince8@gmail.com>
2026-01-17 14:56:12 +0530

    againn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App-minimal';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
 import './styles/theme.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <App />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
